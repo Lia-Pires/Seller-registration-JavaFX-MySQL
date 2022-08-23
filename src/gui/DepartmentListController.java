a733d6b68caf64df27df98d6e0e0a02bd546b074
Lia Pires <liasth.pires@gmail.com>
2022-08-23 15:05:03 -0300

    Create Department List Controller
    Create Department List Controller

---
+++ src/gui/DepartmentListController.java
@@ -0,0 +1,53 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import application.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import model.entities.Department;
+
+public class DepartmentListController implements Initializable {
+
+	@FXML
+	private TableView<Department> tableViewDepartment;
+	
+	@FXML
+	private TableColumn<Department, Integer> tableColumnId;
+	           
+	@FXML
+	private TableColumn<Department, String> tableColumnName;
+	
+	@FXML
+	private Button btNew;
+	
+	@FXML
+	public void onBtNewAction() {
+		System.out.println("OnBtNewAction");
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {	
+		 inicializeNodes();
+	}
+
+	
+	//método auxiliar para iniciar algum componente da tela
+	private void inicializeNodes() {
+		// comando para iniciar apropriadamente o comportamento das colunas da tabela
+		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+		
+		//referenciar o Stage - usar downcast
+		Stage stage = (Stage) Main.getMainScene().getWindow();
+		//comando para a tabela acompanhar o tamanho da janela
+		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());		
+	}
+
+}
